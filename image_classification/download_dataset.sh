f20404b49653c9d1924b414e63436379795bc491
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2021-12-23 18:22:16 +0800

    upadate lars usage

--- image_classification/download_dataset.sh
+++ image_classification/download_dataset.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train_t3.tar
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
 # TODO
